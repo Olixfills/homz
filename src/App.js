022441a8b62c150899a3f7ea885131309ba400bc
Oni Olayiwola A <shasha@pop-os.localdomain>
2022-07-20 16:00:49 +0100

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import NavigationBar from './components/NavigationBar';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Login from './pages/Login';
+import Offers from './pages/Offers';
+// import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <h1>We good fam!</h1>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/reset-password' element={<ForgotPassword />} />
+        </Routes>
+        <NavigationBar />
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
+import { ReactComponent as OffersIcon } from '../assets/svg/localOfferIcon.svg'
+import { ReactComponent as ProfileIcon } from '../assets/svg/personIcon.svg'
+
+
+const NavigationBar = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+
+
+    const activePath = (path) => {
+        if (path === location.pathname){
+            return true
+        }
+        return false
+    }
+
+  return (
+    <footer className='navbar'>
+        <nav className='navbarNav'>
+            <ul className='navbarListItems'>
+                <li className='navbarListItem' onClick={() => navigate('/')}>
+                    <ExploreIcon fill={activePath('/') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' />
+                    <p className={activePath('/') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Explore</p>
+                </li>
+                <li className='navbarListItem' onClick={() => navigate('/offers')}>
+                    <OffersIcon fill={activePath('/offers') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' />
+                    <p className={activePath('/offers') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Offers</p>
+                </li>
+                <li className='navbarListItem' onClick={() => navigate('/profile')}>
+                    <ProfileIcon fill={activePath('/profile') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' />
+                    <p className={activePath('/profile') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Profile</p>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+  )
+}
+
+export default NavigationBar
