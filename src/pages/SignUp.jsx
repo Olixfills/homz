00442f837f9711b10a04463def0644099f8aa98d
Oni Olayiwola A <shasha@pop-os.localdomain>
2022-07-21 16:56:19 +0100

    sign up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,79 +1,99 @@
-import React, {useState} from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
-import visibilityIcon from '../assets/svg/visibilityIcon.svg'
-
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    phone: '',
-    address: '',
-
-  })
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+  });
   // eslint-disable-next-line no-unused-vars
-  const {name, email, password, phone, address} = formData;
+  const { name, email, password, phone } = formData;
   // eslint-disable-next-line no-unused-vars
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-const onChange = (e) => {
-setFormData((prev => ({
-  ...prev,
-  [e.target.id]: e.target.value
-})))
-}
-
+  const onChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  };
 
   return (
     <>
       <div className="pageContainer">
-        <header className='formHeader'>
-          <p className='pageHeader'>Welcome Back!</p>
+        <header className="formHeader">
+          <p className="pageHeader">Welcome Back!</p>
         </header>
-        <form>          
-          <input 
-          type="email" 
-          name="email" 
-          className='emailInput' 
-          id='email' 
-          placeholder='email@email.com' 
-          value={email}  
-          onChange={onChange} />
+        <form>
+          <div className="formDiv">
+            <input
+              type="text"
+              name="name"
+              className="nameInput"
+              id="name"
+              placeholder="Your Name"
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="email"
+              name="email"
+              className="emailInput"
+              id="email"
+              placeholder="email@email.com"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+            <input
+              type="phone"
+              name="phone"
+              className="emailInput"
+              id="phone"
+              placeholder="0700 0000 000"
+              value={phone}
+              onChange={onChange}
+            />
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                id="password"
+                className="passwordInput"
+                placeholder="Password"
+                value={password}
+                onChange={onChange}
+              />
 
-          <div className="passwordInputDiv">
-            <input 
-            type={showPassword ? "text" : "password"} 
-            name="password" 
-            id="password" 
-            className='passwordInput' 
-            placeholder='Password'
-            value={password} 
-            onChange={onChange}/>
-
-            <img src={visibilityIcon} 
-            alt="showPassword" 
-            onClick={()=> setShowPassword(prev => !prev)}
-            className='showPassword'/>
+              <img
+                src={visibilityIcon}
+                alt="showPassword"
+                onClick={() => setShowPassword((prev) => !prev)}
+                className="showPassword"
+              />
           </div>
 
-          <Link to='/reset-password' className='forgotPasswordLink'>Forgot Password?</Link>
+          
 
-          <div className="signInBar">
-            <p className="signInText">Sign In</p>
-            <button className="signInButton">
-              <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+          <div className="signUpBar">
+            <p className="signUpText">Sign Up</p>
+            <button className="signUpButton">
+              <ArrowRightIcon fill="#fff" width="34px" height="34px" />
             </button>
           </div>
         </form>
-        <Link to='/signup' className='registerLink'>Not a member? Sign Up instead</Link>
-
+        <Link to="/signin" className="registerLink">
+          Allready registered? Sign In instead
+        </Link>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default SignUp;
