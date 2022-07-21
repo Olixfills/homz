1ffb0661d3ab907d656509af5844e31e6e14eeb4
Oni Olayiwola A <shasha@pop-os.localdomain>
2022-07-21 16:16:09 +0100

    login screen done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,33 +8,66 @@ import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    name: '',
     email: '',
     password: '',
-    phone: '',
-    address: '',
+    
 
   })
-  const {name, email, password, phone, address} = formData;
+  const { email, password,} = formData;
+  // eslint-disable-next-line no-unused-vars
   const navigate = useNavigate()
 
 const onChange = (e) => {
-
+setFormData((prev => ({
+  ...prev,
+  [e.target.id]: e.target.value
+})))
 }
 
 
   return (
     <>
       <div className="pageContainer">
-        <header>
+        <header className='formHeader'>
           <p className='pageHeader'>Welcome Back!</p>
         </header>
-        <form>
-          <div className='formDiv'>
-          <input type="email" name="email" className='emailInput' id='email' placeholder='Email@email.com' value={email} min={6} onChange={onChange} />
-          <input type="email" name="email" className='emailInput' id='email' placeholder='Email@email.com' value={email} min={6} onChange={onChange} />
+        <form>          
+          <input 
+          type="email" 
+          name="email" 
+          className='emailInput' 
+          id='email' 
+          placeholder='email@email.com' 
+          value={email}  
+          onChange={onChange} />
+
+          <div className="passwordInputDiv">
+            <input 
+            type={showPassword ? "text" : "password"} 
+            name="password" 
+            id="password" 
+            className='passwordInput' 
+            placeholder='Password'
+            value={password} 
+            onChange={onChange}/>
+
+            <img src={visibilityIcon} 
+            alt="showPassword" 
+            onClick={()=> setShowPassword(prev => !prev)}
+            className='showPassword'/>
+          </div>
+
+          <Link to='/reset-password' className='forgotPasswordLink'>Forgot Password?</Link>
+
+          <div className="signInBar">
+            <p className="signInText">Sign In</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+            </button>
           </div>
         </form>
+        <Link to='/signup' className='registerLink'>Not a member? Sign Up instead</Link>
+
       </div>
     </>
   )
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    
+
+  })
+  const { email, password,} = formData;
+  // eslint-disable-next-line no-unused-vars
+  const navigate = useNavigate()
+
+const onChange = (e) => {
+setFormData((prev => ({
+  ...prev,
+  [e.target.id]: e.target.value
+})))
+}
+
+
   return (
-    <div>SignUp</div>
+    <>
+      <div className="pageContainer">
+        <header className='formHeader'>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+        <form>          
+          <input 
+          type="email" 
+          name="email" 
+          className='emailInput' 
+          id='email' 
+          placeholder='email@email.com' 
+          value={email}  
+          onChange={onChange} />
+
+          <div className="passwordInputDiv">
+            <input 
+            type={showPassword ? "text" : "password"} 
+            name="password" 
+            id="password" 
+            className='passwordInput' 
+            placeholder='Password'
+            value={password} 
+            onChange={onChange}/>
+
+            <img src={visibilityIcon} 
+            alt="showPassword" 
+            onClick={()=> setShowPassword(prev => !prev)}
+            className='showPassword'/>
+          </div>
+
+          <Link to='/reset-password' className='forgotPasswordLink'>Forgot Password?</Link>
+
+          <div className="signInBar">
+            <p className="signInText">Sign In</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+        <Link to='/signup' className='registerLink'>Not a member? Sign Up instead</Link>
+
+      </div>
+    </>
   )
 }
 
-export default SignUp
+export default SignUp;
