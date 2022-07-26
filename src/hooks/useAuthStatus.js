8391476f5666775a1d48e4103b2b0b3642087ff5
Oni Olayiwola A <shasha@pop-os.localdomain>
2022-07-26 19:55:53 +0100

    signout option and mount issues solved

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,23 +1,29 @@
-import  {useEffect, useState} from 'react'
+import  {useEffect, useState, useRef} from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 
 export const useAuthStatus = () => {
+    const isMounted = useRef(true)
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [checkStatus, setCheckStatus] = useState(true)
 
     
     useEffect(() => {
-        const auth = getAuth()
-        onAuthStateChanged(auth, (user)=>{
-            if(user){
-                setIsLoggedIn(true)
-            }
-            setCheckStatus(false)
-        })
+        if(isMounted){
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user)=>{
+                if(user){
+                    setIsLoggedIn(true)
+                }
+                setCheckStatus(false)
+            })
+        }
     
+        return ()=>{
+            isMounted.current = false
+        }
     
-    })
+    },[isMounted])
     
 
     return {isLoggedIn, checkStatus}
