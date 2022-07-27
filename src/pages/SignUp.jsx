a13c48ae22c037f517e9da2f7d1f69222c1f6cdc
Oni Olayiwola A <shasha@pop-os.localdomain>
2022-07-27 13:31:18 +0100

    google OAuth on signUp page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,6 +7,7 @@ import {db} from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 import { toast } from 'react-toastify';
+import OAuth from "../components/OAuth";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -139,6 +140,7 @@ const SignUp = () => {
         <Link to="/login" className="registerLink">
           Allready registered? Sign In instead
         </Link>
+        <OAuth />
       </div>
     </>
   );
