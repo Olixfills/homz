a71d03be3024351bea8a966dfb74dfb504fd33a7
Oni Olayiwola A <shasha@pop-os.localdomain>
2022-08-01 23:52:04 +0100

    offers page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import ListngItem from "../components/ListngItem";
 
 const Offers = () => {
-  return (
-    <div>Offers</div>
-  )
-}
+  const [listings, setlistings] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default Offers
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingsRef = collection(db, "listings");
+        const q = query(
+          listingsRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        const querySnap = await getDocs(q);
+
+        let listings = [];
+
+        querySnap.forEach((doc) => {
+          listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setlistings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("could not fetch listings");
+      }
+    };
+
+    fetchListings();
+  }, []);
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">
+          Offers
+        </p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListngItem listing={listing.data} id={listing.id} key={listing.id}/>
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <h4>No offers currently  available</h4>
+      )}
+    </div>
+  );
+};
+
+export default Offers;
